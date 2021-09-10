97cad3c72a797a4aa7b6c0345e3fad2435e1309a
HEKTO <aleksey.yakovlev@gmail.com>
2021-09-10 14:11:59 -0400

    Initial commit

---
+++ BST.hpp
@@ -0,0 +1,99 @@
+/*
+  BST.hpp
+  ------------------------------------------------------------------------------
+  Binary Search Tree
+  ------------------------------------------------------------------------------
+  Ver  1.00 Initial version                                         September 21
+  Ver  2.00 Add copy/move constructors/assignment operators         September 21
+  ------------------------------------------------------------------------------
+  (C) A.Yakovlev, 2021
+*/
+
+#pragma once
+
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+#include "Node.hpp"
+
+template <class KEY>
+struct BST
+{
+
+  BST(): _rootPtr(nullptr)
+  {
+    std::srand(std::time(nullptr));
+  }
+
+  BST(BST const& T): _rootPtr(nullptr)
+  {
+    Copy(&_rootPtr, T._rootPtr);
+  }
+
+  BST(BST&& T): _rootPtr(nullptr)
+  {
+    std::swap(_rootPtr, T._rootPtr);
+  }
+
+  BST& operator=(BST const& T)
+  {
+    if (this != &T)
+    {
+      const auto ptr = _rootPtr;
+      Copy(&_rootPtr, T._rootPtr);
+      Clear(ptr);
+    }
+    return *this;
+  }
+
+  BST& operator=(BST&& T)
+  {
+    if (this != &T)
+    {
+      const auto ptr = _rootPtr;
+      _rootPtr = T._rootPtr;
+      T._rootPtr = nullptr;
+      Clear(ptr);
+    }
+    return *this;
+  }
+
+  virtual ~BST()
+  {
+    this->clear();
+  }
+
+  friend std::ostream& operator<<(std::ostream& S, BST const& T)
+  {
+    return S << T._rootPtr;
+  }
+
+  bool contains(KEY const& K) const noexcept
+  {
+    return Find(K, const_cast<Node<KEY>**>(&_rootPtr)).second;
+  }
+
+  void insert(KEY const& K)
+  {
+    const auto res = Find(K, &_rootPtr);
+    if (not res.second) *res.first = new Node(K);
+  }
+
+  void erase(KEY const& K) noexcept
+  {
+    const auto res = Find(K, &_rootPtr);
+    if (res.second) Erase(res.first);
+  }
+
+  void clear() noexcept
+  {
+    Clear(_rootPtr);
+    _rootPtr = nullptr;
+  }
+
+private:
+
+  Node<KEY>* _rootPtr;
+
+};
---
+++ tc0001.cpp
@@ -0,0 +1,37 @@
+/*
+  tc0001.cpp
+  ------------------------------------------------------------------------------
+  Binary Search Tree Testing
+  ------------------------------------------------------------------------------
+  Ver  1.00 Initial version                                         September 21
+  ------------------------------------------------------------------------------
+  (C) A.Yakovlev, 2021
+*/
+
+#include <iostream>
+#include <random>
+#include <vector>
+
+#include "BST.hpp"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+  // ------ generate randomly ordered vector
+  std::vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+  std::random_device rd;
+  std::mt19937 g(rd());
+  std::shuffle(v.begin(), v.end(), g);
+  // ------ insert the vector into a BST
+  BST<int> t;
+  for (const auto& e: v) t.insert(e);
+  cout << "initial : " << t << endl;
+  // ------ erase BST keys one by one
+  for (const auto& e: v)
+  {
+    t.erase(e);
+    cout << "erase(" << e << "): " << t << endl;
+  }
+}
